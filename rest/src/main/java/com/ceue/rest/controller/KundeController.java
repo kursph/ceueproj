5507addca4b5b8453f5e66f577713a9dbd85ba13
k11718128 <ronald_moritz@yahoo.com>
2020-01-21 19:41:23 +0100

    Format für Präsentation

--- rest/src/main/java/com/ceue/rest/controller/KundeController.java
+++ rest/src/main/java/com/ceue/rest/controller/KundeController.java
@@ -2,10 +2,12 @@ package com.ceue.rest.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 @RestController
 public class KundeController {
 
-    @RequestMapping("/kunde")
+    @RequestMapping(path ="/kunde",  method= POST)
     public String createKunde(@RequestParam(value = "vorname")String vorname, @RequestParam(value="nachname") String nachname){
         return "Kunde " + vorname + " " + nachname + " erstellt. <br /><a href=\"lenker\">Continue here</a>";
     }
